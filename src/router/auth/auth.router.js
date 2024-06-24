23466044a0d8b5b270be885c56b02748846ce7bc
DESKTOP-JEANAGG\ADMIN <cuonghamanhcuong12@gmail.com>
2024-06-24 17:33:20 +0700

    coding refresh access token

--- src/router/auth/auth.router.js
+++ src/router/auth/auth.router.js
@@ -6,9 +6,17 @@ const {
 const {
   validateLoginUser,
 } = require("../../middleware/validates/auth/authLogin.validate");
+const {
+  validateRefreshAccessToken,
+} = require("../../middleware/validates/auth/refreshAccessToken.validate");
 const authRouters = express.Router();
 
 authRouters.post("/register", validateRegisterUser(), authController.register);
 authRouters.post("/login", validateLoginUser(), authController.login);
+authRouters.post(
+  "/RefreshAccessToken",
+  validateRefreshAccessToken(),
+  authController.refreshAccessToken
+);
 
 module.exports = { authRouters };
