398c769d1840d5facd813bcac18253e7d63147a7
cuongkum <cuonghamanhcuong12@gmail.com>
2024-06-30 15:02:31 +0700

    Reapply "coding new feature"
    This reverts commit d3949f4e61a1369073df5253b707eb113574431e.

--- models/LichSuChinhSua.js
+++ models/LichSuChinhSua.js
@@ -1,30 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const NhanVien = require("./NhanVien");
 
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const LichSuChinhSua = sequelize.define('LichSuChinhSua', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
+const LichSuChinhSua = sequelize.define(
+  "LichSuChinhSua",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    SoVanBan: {
+      type: DataTypes.STRING,
+    },
+    NgayThayDoi: {
+      type: DataTypes.DATE,
+    },
+    LinkCu: {
+      type: DataTypes.STRING,
+    },
+    NguoiSua: {
+      type: DataTypes.INTEGER,
+    },
+    NoiDungSua: {
+      type: DataTypes.STRING,
+    },
   },
-  BaoCaoID: {
-    type: DataTypes.INTEGER
-  },
-  NgayThayDoi: {
-    type: DataTypes.DATE
-  },
-  LinkCu: {
-    type: DataTypes.STRING
-  },
-  NguoiSua: {
-    type: DataTypes.INTEGER
-  },
-  NoiDungSua: {
-    type: DataTypes.STRING
+  {
+    tableName: "LichSuChinhSua",
+    timestamps: false,
   }
-}, {
-  tableName: 'LichSuChinhSua',
-  timestamps: false
-});
+);
+NhanVien.hasMany(LichSuChinhSua, { foreignKey: "NguoiSua" });
+LichSuChinhSua.belongsTo(NhanVien, { foreignKey: "NguoiSua" });
 module.exports = LichSuChinhSua;
